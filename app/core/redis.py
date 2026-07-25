368886e692d034fb80cfbd13148c9282d03d9691
toolesstes <toolsgest@gmail.com>
2026-07-25 22:50:37 +0530

    update

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,10 +1,26 @@
+# import os
+# import redis.asyncio as redis
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# redis_client = redis.Redis(
+#     host=os.getenv("REDIS_HOST", "localhost"),
+#     port=int(os.getenv("REDIS_PORT", "6379")),
+#     password=os.getenv("REDIS_PASSWORD"),
+#     ssl=os.getenv("REDIS_TLS", "False").lower() == "true",
+#     decode_responses=True,
+# )
+
+# print("HOST =", os.getenv("REDIS_HOST"))
+# print("PORT =", os.getenv("REDIS_PORT"))
+# print("TLS =", os.getenv("REDIS_TLS"))
+# print("PASSWORD =", bool(os.getenv("REDIS_PASSWORD")))
+
 import os
 import redis.asyncio as redis
 
-redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
-    port=int(os.getenv("REDIS_PORT")),
-    password=os.getenv("REDIS_PASSWORD"),
-    ssl=os.getenv("REDIS_TLS", "False").lower() == "true",
+redis_client = redis.from_url(
+    os.getenv("REDIS_MASTER_URL"),
     decode_responses=True,
 )
