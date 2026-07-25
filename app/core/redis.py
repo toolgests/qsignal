650c8b547876383cc01b9974ff4d60ab77cfd95d
toolesstes <toolsgest@gmail.com>
2026-07-25 22:53:32 +0530

    update

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,26 +1,18 @@
-# import os
-# import redis.asyncio as redis
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# redis_client = redis.Redis(
-#     host=os.getenv("REDIS_HOST", "localhost"),
-#     port=int(os.getenv("REDIS_PORT", "6379")),
-#     password=os.getenv("REDIS_PASSWORD"),
-#     ssl=os.getenv("REDIS_TLS", "False").lower() == "true",
-#     decode_responses=True,
-# )
-
-# print("HOST =", os.getenv("REDIS_HOST"))
-# print("PORT =", os.getenv("REDIS_PORT"))
-# print("TLS =", os.getenv("REDIS_TLS"))
-# print("PASSWORD =", bool(os.getenv("REDIS_PASSWORD")))
-
 import os
 import redis.asyncio as redis
+from dotenv import load_dotenv
 
-redis_client = redis.from_url(
-    os.getenv("REDIS_MASTER_URL"),
+load_dotenv()
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", "6379")),
+    password=os.getenv("REDIS_PASSWORD"),
+    ssl=os.getenv("REDIS_TLS", "False").lower() == "true",
     decode_responses=True,
 )
+
+print("HOST =", os.getenv("REDIS_HOST"))
+print("PORT =", os.getenv("REDIS_PORT"))
+print("TLS =", os.getenv("REDIS_TLS"))
+print("PASSWORD =", bool(os.getenv("REDIS_PASSWORD")))
