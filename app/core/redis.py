eba5df9686b1e8b0e959c98fb275f1cf7b1dda3b
toolesstes <toolsgest@gmail.com>
2026-07-25 22:11:00 +0530

    update

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,7 +1,17 @@
+# import redis.asyncio as redis
+
+# redis_client = redis.Redis(
+#     host="localhost",
+#     port=6379,
+#     decode_responses=True,
+# )
+
+import os
 import redis.asyncio as redis
 
 redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True,
 )
